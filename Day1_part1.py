848e425fb655e062e774464909509625b756cea0
Sindre Milvang <159820301+SindreMilvang@users.noreply.github.com>
2024-12-23 10:18:49 +0100

    Update Day1_part1.py
    Wrote a code that solves part 1 of day 1.

--- Day1_part1.py
+++ Day1_part1.py
@@ -1,1 +1,6 @@
 import numpy as np
+
+content = np.loadtxt("/Users/sindre/Downloads/AoC_day1_input.txt")  #Loads a .txt file as numpy arrays
+list1, list2 = np.split(np.transpose(content), 2)                   #Creates two arrays
+difference = abs(np.sort(list1)-np.sort(list2))                     #Sorts and takes the difference of the arrays
+print(np.sum(difference))                                           #Prints the sum of the differences
