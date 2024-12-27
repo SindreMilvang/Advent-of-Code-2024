23c85ad82ee5c3e41c70fb16d5418ead2f396d1a
Sindre Milvang <159820301+SindreMilvang@users.noreply.github.com>
2024-12-27 21:26:55 +0100

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -6,7 +6,7 @@ difference = abs(np.sort(list1)-np.sort(list2))                     #Sorts and t
 print(np.sum(difference))                                           #Prints the sum of the differences
 
 #Part 2
-similarity_score = 0
-for i in list1[0]:
-    similarity_score += np.count_nonzero(list2[0] == i)*i
+similarity_score = 0                                                
+for i in list1[0]:                                                  
+    similarity_score += np.count_nonzero(list2[0] == i)*i           
 print(similarity_score)
